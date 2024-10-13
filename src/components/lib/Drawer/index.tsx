0e95e280d9d14af9ea7d19192671ae0e37b48f4b
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-13 20:34:02 +0530

    day 2: add drawer component

--- src/components/lib/Drawer/index.tsx
+++ src/components/lib/Drawer/index.tsx
@@ -1,8 +1,18 @@
+import { ReactElement } from "react";
 import { createPortal } from "react-dom";
+import styles from "./Drawer.module.less";
 
-export function Drawer() {
+interface IDrawerProps {
+  children: ReactElement[];
+  onClose: () => void;
+}
+
+export function Drawer({ children, onClose }: IDrawerProps) {
   return createPortal(
-    <div>drawer</div>,
+    <div className={styles.Drawer__root}>
+      <div onClick={onClose} className={styles.Drawer__backdrop} />
+      <div className={styles.Drawer__content}>{children}</div>
+    </div>,
     document.querySelector("#drawer-root")!
   );
 }
--- src/components/section/cart/ViewCartButton/index.tsx
+++ src/components/section/cart/ViewCartButton/index.tsx
@@ -1,9 +1,24 @@
-import { Button } from "../../../lib";
+import { useState } from "react";
+import { Button, Drawer } from "../../../lib";
 
 export function ViewCartButton() {
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const toggleShowDrawer = () => {
+    setShowDrawer((prevShowDrawer) => !prevShowDrawer);
+  };
+
   return (
-    <Button variant="primary" onClick={() => {}}>
-      View Cart
-    </Button>
+    <>
+      <Button variant="primary" onClick={toggleShowDrawer}>
+        View Cart
+      </Button>
+      {showDrawer ? (
+        <Drawer onClose={toggleShowDrawer}>
+          <div>title</div>
+          <div>content</div>
+        </Drawer>
+      ) : null}
+    </>
   );
 }
