9cca577126545b9281f045be7cd06f28005b7535
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-13 14:25:30 +0530

    day 1: add styles to product card and list components

--- src/components/section/products/ProductCard/index.tsx
+++ src/components/section/products/ProductCard/index.tsx
@@ -3,9 +3,15 @@ import styles from "./ProductCard.module.less";
 export function ProductCard() {
   return (
     <div className={styles.ProductCard__root}>
-      <div>Title</div>
-      <div>price</div>
-      <div>content</div>
+      <div className={styles.ProductCard__title}>Title</div>
+      <div className={styles.ProductCard__subtitle}>758.98</div>
+      <div className={styles.ProductCard__content}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        consectetur reiciendis voluptates, iste perspiciatis, ducimus facilis
+        sint, expedita ipsa quod reprehenderit illo ipsam est odio eligendi.
+        Molestiae quas cum sapiente consequatur, eius vel minima rem numquam.
+        Neque alias beatae explicabo.
+      </div>
       <div>actions</div>
     </div>
   );
