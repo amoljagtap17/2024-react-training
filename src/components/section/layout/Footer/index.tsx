f670e5eb2a914d4085594f48f7f3a070e4802739
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-13 13:57:02 +0530

    day 1: add styles to footer component

--- src/components/section/layout/Footer/index.tsx
+++ src/components/section/layout/Footer/index.tsx
@@ -1,5 +1,12 @@
 import styles from "./Footer.module.less";
 
 export function Footer() {
-  return <footer className={styles.Footer__root}>Footer</footer>;
+  return (
+    <footer className={styles.Footer__root}>
+      <div className={styles.Footer__content}>
+        <div>&copy; {new Date().toLocaleDateString()}</div>
+        <div>React Training</div>
+      </div>
+    </footer>
+  );
 }
