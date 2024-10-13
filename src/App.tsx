a7873a4d80831df60d5b5a119ca27e1d1d178ba9
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-13 21:28:33 +0530

    day 2: add lazy routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { lazy, Suspense } from "react";
 import styles from "./App.module.less";
 import { Container, Route } from "./components/lib";
-import { Admin, Home } from "./components/pages";
+
+const Home = lazy(() =>
+  import("./components/pages").then((module) => ({ default: module.Home }))
+);
+
+const Admin = lazy(() =>
+  import("./components/pages").then((module) => ({ default: module.Admin }))
+);
 
 export function App() {
   return (
     <div className={styles.App__root}>
       <Container>
-        <>
+        <Suspense fallback={<p>loading...</p>}>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
-        </>
+        </Suspense>
       </Container>
     </div>
   );
