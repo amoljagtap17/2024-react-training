dd46799081cd1c1f86c96f4b61a73b774b7c9ec7
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-13 16:47:10 +0530

    day 1: add env variables

--- src/components/section/layout/Footer/index.tsx
+++ src/components/section/layout/Footer/index.tsx
@@ -2,12 +2,14 @@ import { Container } from "../../../lib";
 import styles from "./Footer.module.less";
 
 export function Footer() {
+  const appVersion = import.meta.env.VITE_APP_VERSION;
+
   return (
     <footer className={styles.Footer__root}>
       <Container>
         <div className={styles.Footer__content}>
           <div>&copy; {new Date().toDateString()}</div>
-          <div>React Training</div>
+          <div>React Training - version {appVersion}</div>
         </div>
       </Container>
     </footer>
